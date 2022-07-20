5941e3892e70b96a8503a6ede229ffd2f323adba
Oscar Muller <oscarmuller@live.com>
2022-07-21 00:04:38 +0200

    player now parses .csv file to play songs

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 from dotenv import load_dotenv
 import os
+import csv
 
 load_dotenv()
 
@@ -14,24 +15,33 @@ DEVICE_ID = os.getenv("DEVICE_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
+card_song_dict = {}
+
+with open('card-track-dictionairy.csv', mode='r') as f:
+    reader = csv.reader(f)
+    card_song_dict = {rows[0]:rows[1] for rows in reader}
+
 while True:
     try:
         reader=SimpleMFRC522()
-        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                        client_secret=CLIENT_SECRET,
-                                                        redirect_uri="http://localhost:8080",
-                                                        scope="user-read-playback-state,user-modify-playback-state"))
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri="http://localhost:8080",scope="user-read-playback-state,user-modify-playback-state"))
 
         # Create an infinite while loop that will always be waiting for a new scan
         while True:
             print("Waiting for record scan...")
-            id= reader.read()[0]
-            print("Card Value is:",id)
-            sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
-
-            if (id == 557614570277):
-                sp.start_playback(device_id=DEVICE_ID, uris=['spotify:track:4cOdK2wGLETKBW3PvgPWqT'])
+            card_id = reader.read()[0]
+            print("Read succesful! Finding track corresponding to card...")
+            sleep(2)
+            try:
+                song_uri = card_song_dict[str(card_id)]
+                spotify_uri = "spotify:track:" + song_uri
+                print("Track matched to card! Playing...")
+                sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
+                sp.start_playback(device_id=DEVICE_ID, uris=[spotify_uri])
                 sleep(2)
+            except KeyError:
+                print("Unkown card presented! First add card association using the add-card.py script.")
+                pass
 
     # If there is an error, skip it and try the code again (i.e. timeout issues, no active device error, etc)
     except Exception as e:
@@ -39,5 +49,5 @@ while True:
         pass
 
     finally:
-        print("Cleaning  up...")
+        print("Cleaning up...")
         GPIO.cleanup()
